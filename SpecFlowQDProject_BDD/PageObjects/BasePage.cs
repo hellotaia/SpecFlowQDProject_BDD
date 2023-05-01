9048f8ebab9fa554a217b9d3094c2597b39b1aad
Aleksandr Fateev <aleksandr.fateev@sscinc.com>
2023-05-01 08:30:39 +0100

    create BasePage so all Pages could inherit webdriver from it

---
+++ SpecFlowQDProject_BDD/PageObjects/BasePage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace SpecFlowQDProject_BDD.PageObjects
+{
+    public class BasePage
+    {
+        protected IWebDriver driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+}
